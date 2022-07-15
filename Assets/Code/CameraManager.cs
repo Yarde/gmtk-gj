f06894cd0ff609580995f35ce6a60e148a198650
Dawid Kaluza <dawid.kaluza@tensquaregames.com>
2022-07-15 22:35:09 +0200

    camera manger

---
+++ Assets/Code/CameraManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Yarde.Utils.Extensions;
+
+namespace Yarde
+{
+    public class CameraManager : MonoBehaviour
+    {
+        [SerializeField] private Transform transformToFollow;
+        [SerializeField] private float smoothSpeed = 10f;
+        [SerializeField] private Vector3 offset;
+
+        private void FixedUpdate()
+        {
+            Vector3 destination = transformToFollow.position + offset;
+            Vector3 smoothed = Vector3.Lerp(transform.position, destination, smoothSpeed * Time.deltaTime);
+            transform.position = smoothed.WithY(smoothed.y);
+        }
+    }
+}
