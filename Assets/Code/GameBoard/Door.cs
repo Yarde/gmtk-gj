849c27831ce6536341a34d0a265d18d701e7761c
AD\aleksandra.swierczek <aleksandra.swierczek@tensquaregames.com>
2022-07-16 22:21:27 +0200

    set sprite basing on damage

--- Assets/Code/GameBoard/Door.cs
+++ Assets/Code/GameBoard/Door.cs
@@ -1,11 +1,20 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
 using VContainer;
 
 namespace Yarde.GameBoard
 {
     public class Door : ObstacleBase
     {
+        [SerializeField] private List<Sprite> sprites;
         [Inject] private Player _player;
+
+        protected void Start()
+        {
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null && sprites.Count > 0) spriteRenderer.sprite = sprites[(int)Mathf.Max(Hp - 1, 0)];
+        }
 
         public override UniTask OnTouch()
         {
