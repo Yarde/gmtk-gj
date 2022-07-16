65a39531cb7b00d23d0f89821e4997516162afb8
swrczk <45397673+swrczk@users.noreply.github.com>
2022-07-16 15:52:10 +0200

    2 types of enemies (#1)
    * enemy base
    
    * path following enemies
    
    * plane prefab
    
    * rename
    
    * enemy following player
    
    * awesome code review
    
    Co-authored-by: AD\aleksandra.swierczek <aleksandra.swierczek@tensquaregames.com>

---
+++ Assets/Code/GameBoard/Enemies/EnemyFollowingPath.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Yarde.GameBoard.Enemies
+{
+    public class EnemyFollowingPath : EnemyBase
+    {
+        [SerializeField] private List<Transform> waypoints;
+        [SerializeField] private float moveDelayInSec = 1;
+        [SerializeField] private float movementSpeed = 1;
+        private Vector3 _targetPosition;
+        private int _queueDirection = 1;
+        private int _waypointIndex;
+
+        private void Start()
+        {
+            SetInitValues();
+            CheckWaypoint();
+        }
+
+        private void SetInitValues()
+        {
+            transform.position = waypoints[_waypointIndex].position;
+            _waypointIndex++;
+            _targetPosition = waypoints[_waypointIndex].position;
+        }
+
+        private void CheckWaypoint()
+        {
+            if (_waypointIndex == waypoints.Count - 1)
+                _queueDirection = -1;
+            if (_waypointIndex == 0)
+                _queueDirection = 1;
+            if (transform.position == _targetPosition)
+            {
+                _waypointIndex += _queueDirection;
+                _targetPosition = waypoints[_waypointIndex].position;
+            }
+        }
+
+        public override Vector3 GetEnemyMove()
+        {
+            return Vector3.MoveTowards(transform.position, _targetPosition, movementSpeed);;
+        }
+
+        public override bool CheckPlayerHit(Vector3 direction)
+        {
+            //TO DO implement
+            return false;
+        }
+
+        public override UniTask Kill()
+        {
+            Destroy(gameObject);
+            return UniTask.CompletedTask;
+        }
+
+        public override async UniTask MakeMove(Vector3 direction)
+        {
+            await transform.DOMove(direction, moveDelayInSec);
+            CheckWaypoint();
+        }
+
+        public override async UniTask MakeHalfMove(Vector3 direction)
+        {
+            var prevPosition = transform.position;
+            var newDirection = Vector3.MoveTowards(prevPosition, direction, 0.5f);
+            await transform.DOMove(newDirection, moveDelayInSec/2);
+            await transform.DOMove(prevPosition, moveDelayInSec/2);
+        }
+    }
+}
---
+++ Assets/Code/GameBoard/Enemies/EnemyFollowingPlayer.cs
@@ -0,0 +1,47 @@
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using VContainer;
+using Yarde.GameBoard;
+
+namespace Yarde
+{
+    public class EnemyFollowingPlayer : EnemyBase
+    {
+        [SerializeField] private float moveDelayInSec = 1;
+        [SerializeField] private float movementSpeed = 1;
+        [Inject] private Player _player;
+        private Vector3 _targetPosition;
+        private int _waypointIndex;
+
+        public override Vector3 GetEnemyMove()
+        {
+            _targetPosition = _player.transform.position;
+            return Vector3.MoveTowards(transform.position, _targetPosition, movementSpeed);
+        }
+
+        public override bool CheckPlayerHit(Vector3 direction)
+        {
+            return false;
+        }
+
+        public override UniTask Kill()
+        {
+            Destroy(gameObject);
+            return UniTask.CompletedTask;
+        }
+
+        public override async UniTask MakeMove(Vector3 direction)
+        {
+            await transform.DOMove(direction, moveDelayInSec);
+        }
+
+        public override async UniTask MakeHalfMove(Vector3 direction)
+        {
+            var prevPosition = transform.position;
+            var newDirection = Vector3.MoveTowards(prevPosition, direction, 0.5f);
+            await transform.DOMove(newDirection, moveDelayInSec / 2);
+            await transform.DOMove(prevPosition, moveDelayInSec / 2);
+        }
+    }
+}
